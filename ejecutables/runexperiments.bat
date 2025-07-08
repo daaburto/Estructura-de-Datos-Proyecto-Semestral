8e1b84c66de04a32ef51673f0fe6d0971f5d355f
joacosando <joaquinsandovalreyes04@gmail.com>
2025-07-08 02:28:58 -0400

    feat: Cambios en la prueba de suffix-tree

--- ejecutables/runexperiments.bat
+++ ejecutables/runexperiments.bat
@@ -2,7 +2,7 @@
 echo ejecutable,tiempo
 
 for /L %%c in (1,1,20) do (
-    for %%n in (tests\80\*) do (
-        kmp.exe %%n
+    for %%n in (tests\10\*) do (
+        suffix-tree.exe %%n
     )
 )
--- runexperiments.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ejecutable;tamanoArchivo;tiempo
-
-for /L %%c in (1,1,20) do (
-    for %%n in (ejecutables\tests\DNA\*) do (
-        boyer-moore.exe %%n
-    )
-)
